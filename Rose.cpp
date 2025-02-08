4fa5f75387fd400e5ee83ad215d2e10edee1e646
Seth Kapanda <sethgraphicstudio@gmail.com>
2025-02-08 16:45:42 +0200

    Rose.cpp
    Updated the code to allow the user three tries before the program stops.

--- Rose.cpp
+++ Rose.cpp
@@ -1,101 +1,110 @@
 #include <iostream>
 #include <string>
-#include <fstream>
+#include <cctype> // for tolower
 using namespace std;
 
-void guessRose (){
-    cout<< "Main menue: "<<endl;
-    cout<< "1. Start Game"<< endl;
-    cout<< "2. End game"<< endl;
+void guessRose() {
+    cout << "Main menu: " << endl;
+    cout << "1. Start Game" << endl;
+    cout << "2. End game" << endl;
+}
 
+void inGame() {
+    cout << "\nDo you know Rose?" << endl;
+    cout << "Roses's favourite color?" << endl;
+    cout << "a. Red | b. Pink | c. Purple" << endl;
+}
 
-}//end of function
+void birthDate() {
+    cout << "When was Rose born?" << endl;
+    cout << "a. 2005/17/06 | b. 2005/16/04 | c. 2005/16/09" << endl;
+}
 
-void inGame (){
-    cout<< "\nDo you know Rose?"<< endl;
-    cout<< " Roses's favourite color? "<< endl;
-    cout<< "a. Red | b. Pink | c. Purple" <<endl;
+void correctAnswer() {
+    cout << "Correct, you move on to the next round\n" << endl;
+}
 
-}//end of game function
+void wrongAnswer() {
+    cout << "Wrong answer!" << endl;
+}
 
-void birthDate(){
-    cout<< "When was Rose born?"<< endl;
-    cout<< "a. 2005/17/06 | b. 2005/16/04 | c. 2005/16/09"<< endl;
+void finalQuestion() {
+    cout << "Where does Rose want to spend her 20th birthday?" << endl;
+    cout << "a. Singapore | b. Paris | c. Dubai" << endl;
+}
 
-}//end of DOB function
-
-void correctAnswer(){
-    cout<< "Correct, you move on to the next round\n"<< endl;
-
-}//end of feedback
-
-void wrongAnswer(){
-    cout<< "Wrong answer!"<< endl;
-
-}//end of wrong feedback
-
-void finalQuestion(char choice){
-    cout<< "Where does Rose want to spend her 20th birthday?"<< endl;
-    cout<< "a. Singapore | b. Paris | c. Dubai"<< endl;
-
-     //getting user input from the choice variable
-     choice = tolower(choice);
-     cin>> choice;
-      if (choice == 'b'){
-        cout<< "Well done, you've made it to the end of the game...Have a lovely day!"<< endl;
-
-      } else{
-        wrongAnswer();
-      }
-
-}//end of final question
-
-
-
-int main(){
-    int choice= 0;
-    int number_of_chances = 0; //number of tries per question
+int main() {
+    int choice = 0;
+    int number_of_chances = 0; // number of tries per question
 
     guessRose();
-    cin>> choice;
+    cin >> choice;
 
-     switch (choice){
+    switch (choice) {
         case 1: {
-                char first_question, second_question, third_question;
+            char first_question, second_question, third_question;
+            while (true) {
+                // Reset number_of_chances for the first question
+                number_of_chances = 0;
                 inGame();
-                cin>> first_question;
+                cin >> first_question;
                 first_question = tolower(first_question);
-                 if (first_question == 'c'){
+                if (first_question == 'c') {
                     correctAnswer();
 
-                    //second question
+                    // Reset number_of_chances for the second question
+                    number_of_chances = 0;
                     birthDate();
-                    cin>> second_question;
-                     if (second_question == 'c'){
+                    cin >> second_question;
+                    second_question = tolower(second_question);
+                    if (second_question == 'c') {
                         correctAnswer();
-                         finalQuestion(third_question);
-                     } else{
+
+                        // Reset number_of_chances for the final question
+                        number_of_chances = 0;
+                        finalQuestion();
+                        cin >> third_question;
+                        third_question = tolower(third_question);
+                        if (third_question == 'b') {
+                            correctAnswer();
+                            cout << "You've gotten everything right! Bye!" << endl;
+                            break;
+                        } else {
+                            wrongAnswer();
+                            number_of_chances++;
+                            if (number_of_chances == 3) {
+                                cout << "You've used up all your chances, Goodbye!" << endl;
+                                break;
+                            }
+                        }
+                    } else {
                         wrongAnswer();
+                        number_of_chances++;
+                        if (number_of_chances == 3) {
+                            cout << "You've used up all your chances! Goodbye" << endl;
+                            break;
+                        }
+                    }
+                } else {
+                    wrongAnswer();
+                    number_of_chances++;
+                    if (number_of_chances == 3) {
+                        cout << "You've used up all your chances! Goodbye!!" << endl;
                         break;
-                     }//end of second question if
-                    
-
-
-                 } else{
-                   wrongAnswer();
-                   break;
-                 }//end of first if
-                break;
-
-        }//end of case 1
-
+                    }
+                }
+            }
+            break; // end of case 1
+        }
         case 2: {
-            cout<< "Game ended"<< endl;
+            cout << "Game ended" << endl;
             break;
+        }
+        default: {
+            cout << "Invalid choice" << endl;
+            break;
+        }
+    }
 
-        }//end of case 2
-
-     }//end of switch statement
-     
     return 0;
 }
